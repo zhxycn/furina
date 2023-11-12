df624339a8555d22de9a99f226c350b968b64286
zhxy-CN <admin@owo.cab>
2023-11-12 17:50:12 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <img className="block mx-auto h-auto max-h-96 sm:mx-0" src="/furina.webp" alt="Furina"/>
+    </main>
+  )
+}
